7aac90b2fdcbe520a99e08274dac3a74acee2bf9
Robert Castellano <castellanorob@gmail.com>
2018-02-26 11:42:54 -0600

    Updated friends.js, server.js

---
+++ app/data/friends.js
@@ -0,0 +1,5 @@
+// Create friends array
+
+var friends = {
+    
+}
---
+++ server.js
@@ -0,0 +1,18 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
