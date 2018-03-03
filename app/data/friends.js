f9bc0dd0b119927bf00f819d101eaef6c2dae73c
Robert Castellano <castellanorob@gmail.com>
2018-03-03 01:27:36 -0600

    Updated HTML files, still working on API routing

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,8 +2,84 @@
 
 var friends = [
     {
-     name: "Carl Sagan",
-     photo: "https://goo.gl/images/wqzweY",
-      
+      "name": "Carl Sagan",
+      "photo": "https://goo.gl/images/wqzweY",
+      "scores": [
+        3,
+        1,
+        4,
+        5,
+        2,
+        1,
+        2,
+        5,
+        4,
+        4
+  
+     ] 
     },
-]
+    {
+      "name": "Stephen Hawking",
+      "photo": "https://goo.gl/images/WK9r8n",
+      "scores": [
+        1,
+        4,
+        3,
+        5,
+        3,
+        5,
+        1,
+        4,
+        5,
+        4
+      ]
+    },
+    {
+      "name": "Margaret Hamilton",
+      "photo": "https://goo.gl/images/S8qdp1",
+      "scores": [
+          5,
+          2,
+          5,
+          2,
+          4,
+          5,
+          1,
+          3,
+          1,
+          5
+        ]
+    },
+    { "name": "Rosalind Franklin",
+      "photo": "https://goo.gl/images/sRdXJV",
+      "scores": [
+        1,
+        5,
+        2,
+        5,
+        3,
+        5,
+        2,
+        4,
+        3,
+        1
+      ]
+    },
+    { "name": "Nina Tandon",
+    "photo": "https://goo.gl/images/BsZSGW",
+    "scores": [
+      3,
+      2,
+      4,
+      1,
+      5,
+      5,
+      5,
+      1,
+      4,
+      2
+    ]
+  },
+];
+
+module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,7 @@
 // Dependencies
 
 var path = require("path");
-var friendData = require("..data/friends.js");
+var friendData = require(".app/data/friends.js");
 
 
 module.exports = function(app) {
@@ -9,5 +9,33 @@ module.exports = function(app) {
       res.json(friends);
     })
 
+app.post("/api/friends", function(req, res) {
+    var userInput = req.body;
+    var userResponses = userInput.scores;
+    var matchName = ""; 
+    var matchImage = "";
+    var totalDifference = 10000;
 
+    for (var i = 0; i < friends.length; i++) {
+       var diff = 0;
+
+
+      for (var j = 0; j < userResponses.length; j++) {
+        diff += Math.abs(friends[i].scores[j] - userResponses[j]);
+      }
+  
+
+      if (diff < totalDifference) {
+        totalDifference = diff;
+        matchName = friends[i].name;
+        matchImage = friends[i].photo;
+      }
+    };
+
+    // Adding a new user to the array
+    friends.push(userInput);
+
+    // Send appropriate response
+    res.json({ status: "ok", matchName: matchName, matchImage: matchImage });
+  });
 };
